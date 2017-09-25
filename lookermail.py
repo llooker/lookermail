8c93c9ea11673d7beb0aab9076d43ebdf846fa25
russlooker <russell@looker.com>
2017-09-25 12:21:31 -0700

    Working Pre-modularization
    working with modularization

--- lookermail.py
+++ lookermail.py
@@ -2,7 +2,17 @@
 import requests
 import ConfigParser
 
-#Read config file with Looker API and Database connection information
+###### EMAIL IMPORTS #####
+import smtplib, os, sys, datetime, time
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
+import jinja2
+import codecs
+
+#Read config file with Looker API and SMTP gateway information
 config = ConfigParser.RawConfigParser(allow_no_value=True)
 config.read('configs/config.txt')
 
@@ -33,22 +43,87 @@ class lookerAPIClient:
         return self.get('/'.join(['looks',look,'run','json'])+optional_arguments)
 
 
+class email:
+    def __init__(self):
+        print 'email class instantiated'
+
+    def send_html_mail(self, send_from, send_to, subject, html, files=[], send_to_cc=[]):
+        assert type(send_to)==list
+        assert type(files)==list
+        text = ''
+        msg = MIMEMultipart('alternative')
+        msg['From'] = send_from
+        msg['To'] = COMMASPACE.join(send_to)
+        msg['Cc'] = COMMASPACE.join(send_to_cc)
+        msg['Date'] = formatdate(localtime=True)
+        msg['Subject'] = subject
+
+        msg.attach( MIMEText(text, 'plain'))
+        msg.attach( MIMEText(html.encode('UTF-8'), 'html'))
+        
+        for f in files:
+            part = MIMEBase('application', "octet-stream")
+            part.set_payload( open(f,"rb").read() )
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
+            msg.attach(part)
+
+        # smtp = smtplib.SMTP(server)
+        smtp = smtplib.SMTP_SSL(config.get('smtp', 'host'), config.getint('smtp', 'port'))
+        # smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp.ehlo()
+        smtp.login(config.get('smtp', 'username'), config.get('smtp', 'password'))
+        # smtp.starttls()
+        # smtp.sendmail(send_from, lib.modules.abstract.removeDupes(send_to + send_to_cc), msg.as_string())
+        smtp.sendmail(send_from, send_to + send_to_cc, msg.as_string())
+        smtp.close()
+
+    def build_message(self, TEMPLATE_FILE, templateVars):
+        templateLoader = jinja2.FileSystemLoader(searchpath = "templates/")
+        # templateLoader = jinja2.FileSystemLoader(searchpath = os.getcwd() + "/lib/templates/") ## Original Line
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        template = templateEnv.get_template( TEMPLATE_FILE )
+        return template.render( templateVars )
 
 class execution:
     def __init__(self):
         print 'Execution Initiated'
 
     def run(self):
-        x = lookerAPIClient(
-            api_host      = config.get('api', 'api_host'), 
-            api_client_id = config.get('api', 'api_client_id'), 
-            api_secret    = config.get('api', 'api_secret'), 
-            api_port      = config.get('api', 'api_port')
+        lookerAPIInstance = lookerAPIClient(
+            api_host      = config.get('api', 'host'), 
+            api_client_id = config.get('api', 'client_id'), 
+            api_secret    = config.get('api', 'secret'), 
+            api_port      = config.get('api', 'port')
         )
-        historicalCustomers = x.runLook('292',limit=10000)
-        newCustomers = x.runLook('293',limit=10000)
-        print historicalCustomers
-
+        basicTotal = lookerAPIInstance.runLook('299',limit=10000)
+        clientSummary = lookerAPIInstance.runLook('349',limit=10000)
+        emailCoordinatorInstance = email()
+        # message_variables = {
+        # "SENDDATE":time.strftime("%m/%d/%Y"),
+        # "URLDATE":time.strftime("%m-%d-%Y"),
+        # "MESSAGETITLE":'Hello World!',
+        # "MESSAGETEXT":'message with good news!',
+        # }
+        # testfile = codecs.open('cool.html','w',"utf-8")
+        # testfile.write( y.build_message('default.html',message_variables) )
+        templateVars = {
+        "SENDDATE":time.strftime("%m/%d/%Y"),
+        "URLDATE":time.strftime("%m-%d-%Y"),
+        "MESSAGETITLE":'Donation Report',
+        "MESSAGETEXT":'Donation Report Body Text',
+        "clients":clientSummary,
+        "basictotal":basicTotal
+        }
+        emailCoordinatorInstance.send_html_mail(
+            'russell@looker.com', #from 
+            ['russelljgarner@gmail.com'], #to
+            'Daily Donation and Volunteer Report', #subject line
+            emailCoordinatorInstance.build_message("donation_report/donation_report.html",templateVars), #Template Variables
+            [] #Attachements (optional)
+        )
+        # emailCoordinatorInstance.simpleNotice('hello from lookermail script',historicalCustomers[0]['ltv_predictions.sum_ltv_prediction'],'Test',['russelljgarner@gmail.com'])
+        # testfile.close()
 
 instance = execution()
 instance.run()
